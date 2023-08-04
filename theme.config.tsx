6e3c8da0955d91c9fbc87b5ec4cd3fe16edf1f06
xspqwe <99954498+xspqwe@users.noreply.github.com>
2023-08-04 17:36:35 +0800

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,9 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: <span>AI小巴士文档</span>,
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'AI小巴士',
   },
 }
 
